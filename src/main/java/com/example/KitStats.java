46d37efb2f521223310048317f024d1f589f2837
bilalseptember2011-dot <bilalseptember2011@gmail.com>
2026-04-05 23:49:17 +0500

    Add KitStats class to manage player statistics

---
+++ src/main/java/com/example/KitStats.java
@@ -0,0 +1,17 @@
+package com.pvptier.data;
+
+public class KitStats {
+    public int rating = 1500;
+    public int matches = 0;
+    public int wins = 0;
+    public int losses = 0;
+    public int totalHits = 0;
+    public int totalAttacks = 0;
+    public int combos = 0;
+    public int maxCombo = 0;
+    public int currentCombo = 0;
+    public int crits = 0;
+    public int opponentHits = 0;
+    public int lastDamageTime = 0;
+    public boolean inMatch = false;
+}
